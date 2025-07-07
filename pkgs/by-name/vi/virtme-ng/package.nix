9820667feae3e831a112a13a0a18c3bd8d03f696
Jake Hillion <jake@hillion.co.uk>
2025-07-07 18:55:01 +0100

    virtme-ng: init at 1.36

--- pkgs/by-name/vi/virtme-ng/package.nix
+++ pkgs/by-name/vi/virtme-ng/package.nix
@@ -13,19 +13,23 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "virtme-ng";
-  version = "1.33";
+  version = "1.36";
 
   src = fetchFromGitHub {
     owner = "arighi";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-JPcjOHS0rAbzNGVUMnMJKG/WzLXBejWF6oCRoDNXFRI=";
+    sha256 = "sha256-/dO7BAWLsmL/EVO/9Ja8h7MuYTwOxBav3iHQkegyyoY=";
   };
+
+  pyproject = true;
+  build-system = with python3.pkgs; [ setuptools ];
 
   nativeBuildInputs = [ makeWrapper ];
 
   propagatedBuildInputs = with python3.pkgs; [
     argcomplete
+    argparse-manpage
     requests
     setuptools
   ] ++ [
@@ -35,15 +39,15 @@ python3.pkgs.buildPythonApplication rec {
   ];
 
   postFixup = ''
-    mv $out/lib/python3.12/site-packages/virtme/guest/virtme-init{,.unwrapped}
+    mv $out/lib/python3.13/site-packages/virtme/guest/virtme-init{,.unwrapped}
 
-    substituteInPlace $out/lib/python3.12/site-packages/virtme/guest/virtme-init.unwrapped \
+    substituteInPlace $out/lib/python3.13/site-packages/virtme/guest/virtme-init.unwrapped \
       --replace-fail "/bin/bash" "${bash}/bin/bash" \
       --replace-fail "export PATH=" "export PATH=\$PATH:" \
-      --replace-fail "udevd=\`which udevd\`" "udevd=${systemd}/lib/systemd/systemd-udevd" \
+      --replace-fail "udevd=\$(command -v udevd)" "udevd=${systemd}/lib/systemd/systemd-udevd" \
       --replace-fail "setsid bash -c \"su " "setsid bash -c \"su -s ${bash}/bin/bash "
 
-    makeWrapper $out/lib/python3.12/site-packages/virtme/guest/virtme-init{.unwrapped,} \
+    makeWrapper $out/lib/python3.13/site-packages/virtme/guest/virtme-init{.unwrapped,} \
       --prefix PATH : ${lib.makeBinPath [
           bash
           busybox # slimmed down coreutils
